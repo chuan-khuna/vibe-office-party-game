306a6061a4d584458fd45fbeb0c12d9cec8c5d6c
ALTR <chuan.khuna@gmail.com>
2025-06-17 11:23:47 +0700

    show image

--- office_game_app/pages/whats_behind.py
+++ office_game_app/pages/whats_behind.py
@@ -1,6 +1,11 @@
 import reflex as rx
+from PIL import Image
+
+place_holder_image_path = 'assets/lucy.png'
 
 
 def whats_behind() -> rx.Component:
     # what's behind game
-    return rx.container(rx.text("What's Behind Game"))
+    image = Image.open(place_holder_image_path)
+
+    return rx.container(rx.vstack(rx.text("What's Behind Game"), rx.image(src=image)))
