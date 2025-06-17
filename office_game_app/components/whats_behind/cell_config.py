9eec52eb610f30c12bb21035689ce3c536e32fa9
ALTR <chuan.khuna@gmail.com>
2025-06-17 21:57:13 +0700

    change button colour

--- office_game_app/components/whats_behind/cell_config.py
+++ office_game_app/components/whats_behind/cell_config.py
@@ -14,18 +14,18 @@ def upload_input(state: rx.State) -> rx.Component:
                     border=f"1px solid {color}",
                 ),
                 rx.text("Drag and drop files here or click to select files"),
+                rx.hover_card.root(
+                    rx.hover_card.trigger(rx.link("Hover to view uploaded file path")),
+                    rx.hover_card.content(
+                        rx.text(rx.selected_files("upload1")),
+                        side="top",
+                    ),
+                ),
             ),
             id="upload1",
             max_files=1,
             border=f"1px dotted {color}",
             padding="5em",
-        ),
-        rx.hover_card.root(
-            rx.hover_card.trigger(rx.text("uploaded file path")),
-            rx.hover_card.content(
-                rx.text(rx.selected_files("upload1")),
-                side="top",
-            ),
         ),
         rx.hstack(
             rx.button(
@@ -33,8 +33,9 @@ def upload_input(state: rx.State) -> rx.Component:
                 on_click=state.handle_upload(rx.upload_files(upload_id="upload1")),
             ),
             rx.button(
-                "Clear",
+                "Remove Selected File",
                 on_click=rx.clear_selected_files("upload1"),
+                color_scheme="red",
             ),
         ),
     )
@@ -47,30 +48,34 @@ def cell_config_input(state: rx.State) -> rx.Component:
                 rx.heading(f"{state.num_cols} x {state.num_rows}"),
                 rx.vstack(
                     rx.text("Number of Rows"),
-                    rx.slider(min=3, max=10, value=[state.num_rows], on_change=state.set_num_rows.throttle(20), width="200px"),
+                    rx.slider(
+                        min=3, max=10, value=[state.num_rows], on_change=state.set_num_rows.throttle(20), width="200px"
+                    ),
                 ),
                 rx.vstack(
                     rx.text("Number of Columns"),
-                    rx.slider(min=3, max=10, value=[state.num_cols], on_change=state.set_num_cols.throttle(20), width="200px"),
+                    rx.slider(
+                        min=3, max=10, value=[state.num_cols], on_change=state.set_num_cols.throttle(20), width="200px"
+                    ),
                 ),
-                rx.button("Reset State", on_click=state.reset_cell, width="200px"),
+                rx.button("Reset State", on_click=state.reset_cell, width="200px", color_scheme="cyan"),
                 rx.button(
                     "Reset Game to Default",
                     on_click=state.reset_game_to_default,
                     width="200px",
-                    color_scheme="red",
+                    color_scheme="cyan",
                 ),
                 rx.hstack(
                     rx.button(
                         "Show all",
-                        on_click=state.show_all,
-                        width="80px",
+                        on_double_click=state.show_all,
+                        width="50%",
                         color_scheme="gray",
                     ),
                     rx.button(
                         "Hide all",
                         on_click=state.hide_all,
-                        width="80px",
+                        width="50%",
                         color_scheme="gray",
                     ),
                 ),
