27dd30b0ab8d4195829b13bad5a9228f83dc58f6
ALTR <chuan.khuna@gmail.com>
2025-06-17 11:14:04 +0700

    add page

--- office_game_app/office_game_app.py
+++ office_game_app/office_game_app.py
@@ -4,6 +4,8 @@ import reflex as rx
 
 from rxconfig import config
 from office_game_app.pages.index import index
+from office_game_app.pages.whats_behind import whats_behind
 
 app = rx.App()
 app.add_page(index, "/")
+app.add_page(whats_behind, "/whats_behind")
---
+++ office_game_app/pages/whats_behind.py
@@ -0,0 +1,6 @@
+import reflex as rx
+
+
+def whats_behind() -> rx.Component:
+    # what's behind game
+    return rx.container(rx.text("What's Behind Game"))
