c88010a462d3d366afdce1daeeb8cdf972f18f8d
ALTR <chuan.khuna@gmail.com>
2025-06-17 13:12:34 +0700

    refactor: enhance image grid functionality with hidden state management

--- office_game_app/components/whats_behind/image_grid.py
+++ office_game_app/components/whats_behind/image_grid.py
@@ -1,24 +1,41 @@
 import reflex as rx
 from PIL import Image
 import logging
+import numpy as np
 
 GAP_SIZE = "2px"
 
 
-def wrap_image(image: Image.Image, hidden: bool = False) -> rx.Component:
-    # if hidden display it with grayscale
+def _styled_image(image: Image.Image, hidden: bool = False, on_click=None) -> rx.Component:
+    style = {"width": "100%", "height": "100%", "cursor": "pointer"}
     if hidden:
-        return rx.image(src=image, style={"filter": "grayscale(100%)", "opacity": "1.0"})
-    return rx.image(src=image)
+        # hidden effect
+        # style.update({"filter": "grayscale(80%)", "opacity": "1.0"})
+
+        # pillow image to black colour
+        arr = np.zeros_like(np.array(image))
+        image = Image.fromarray(arr)
+
+    return rx.image(src=image, style=style, on_click=on_click)
 
 
-def image_grid(image: Image.Image, num_rows: int, num_cols: int) -> rx.Component:
+def wrap_image(image: Image.Image, row_index: int, col_index: int, state: rx.State) -> rx.Component:
+    hidden = state.cells[f"{row_index}_{col_index}"]
+    return rx.cond(
+        hidden,
+        _styled_image(image, hidden=True, on_click=state.toggle_hidden(row=row_index, col=col_index)),
+        _styled_image(image, hidden=False, on_click=state.toggle_hidden(row=row_index, col=col_index)),
+    )
+
+
+def image_grid(image: Image.Image, num_rows: int, num_cols: int, state: rx.State) -> rx.Component:
     # divide image into grid cells
     logging.info(f"image size: {image.size}, rows: {num_rows}, cols: {num_cols}")
 
     # resize image to have max width and height
     max_size = (800, 800)
     image.thumbnail(max_size)
+    image = image.convert("RGB")  # ensure image is in RGB mode
 
     cell_width = image.width // num_cols
     cell_height = image.height // num_rows
@@ -40,9 +57,13 @@ def image_grid(image: Image.Image, num_rows: int, num_cols: int) -> rx.Component
         rx.vstack(
             *[
                 rx.hstack(
-                    *[wrap_image(image=cell_image, hidden=True) for cell_image in row_cells], style={"gap": GAP_SIZE}
+                    *[
+                        wrap_image(image=cell_image, row_index=row_index, col_index=col_index, state=state)
+                        for col_index, cell_image in enumerate(row_cells)
+                    ],
+                    style={"gap": GAP_SIZE},
                 )
-                for row_cells in image_grid
+                for row_index, row_cells in enumerate(image_grid)
             ],
             style={"gap": GAP_SIZE},
         ),
--- office_game_app/pages/whats_behind.py
+++ office_game_app/pages/whats_behind.py
@@ -4,10 +4,34 @@ from PIL import Image
 from office_game_app.components.whats_behind.image_grid import image_grid
 
 place_holder_image_path = 'assets/lucy.png'
+default_num_rows = 5
+default_num_cols = 5
+
+
+class CellState(rx.State):
+    num_rows: int = default_num_rows
+    num_cols: int = default_num_cols
+    cells: dict[str, bool] = {
+        f"{row}_{col}": True for row in range(default_num_rows) for col in range(default_num_cols)
+    }
+
+    @rx.event
+    def toggle_hidden(self, row: int, col: int):
+        # toggle the hidden state of a cell
+        key = f"{row}_{col}"
+        if key in self.cells:
+            self.cells[key] = not self.cells[key]
+        else:
+            rx.log(f"Cell {key} not found in state.")
 
 
 def whats_behind() -> rx.Component:
     # what's behind game
     image = Image.open(place_holder_image_path)
 
-    return rx.container(rx.vstack(rx.text("What's Behind Game"), image_grid(image, 5, 5)))
+    return rx.container(
+        rx.vstack(
+            rx.text("What's Behind Game"),
+            image_grid(image, num_rows=default_num_rows, num_cols=default_num_cols, state=CellState),
+        )
+    )
